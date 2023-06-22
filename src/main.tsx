35c2014c987f4c61feaa50595a3b65d3b7c65325
Leonardo Araujo <leo.san9@hotmail.com>
2023-06-22 11:33:42 -0300

    change sign in to react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { AuthProvider } from "./contexts/AuthContext/index.tsx";
 import { MenuProvider } from "./contexts/SidebarContext/index.tsx";
 import { QueryClientProvider } from "react-query";
 import "./i18n.ts";
@@ -9,12 +8,10 @@ import { queryClient } from "./services/queryClient.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
-    <AuthProvider>
-      <MenuProvider>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </MenuProvider>
-    </AuthProvider>
+    <MenuProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </MenuProvider>
   </QueryClientProvider>
 );
