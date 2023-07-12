11bc8b611c559b33920b21fc2e8c6c6cd0b197e9
Leonardo Araujo <leo.san9@hotmail.com>
2023-07-12 16:48:36 -0300

    redirect start

--- src/routes/private/redirect.tsx
+++ src/routes/private/redirect.tsx
@@ -1,15 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { queryClient } from "@src/services/queryClient";
 import { useValidate } from "@src/services/siginIn/validate";
+import { ValidateInterface } from "@src/services/types/validate.interface";
 import { Spin } from "antd";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Redirect = () => {
+  const { permissions } = queryClient.getQueryData(
+    "validate"
+  ) as ValidateInterface;
   const { responseValidate } = useValidate();
   const navigate = useNavigate();
 
   useEffect(() => {
-    responseValidate?.type === 3
+    permissions.report.paybrokers.balance.report_paybrokers_balance_list
+      ? navigate("/consult/consult_organization/organization_balance")
+      : permissions.report.paybrokers.extract.report_paybrokers_extract_list
+      ? navigate("/consult/consult_organization/organization_bank_statement")
+      : responseValidate?.type === 3
       ? navigate("/consult/consult_merchant/merchant_bank_statement")
       : navigate("/consult/deposit/generated_deposits");
   }, []);
