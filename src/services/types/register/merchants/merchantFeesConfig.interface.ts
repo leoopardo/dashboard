36fc8fa346b8ec16694a8e3a3c320c33581bc0cb
Leonardo Araujo <leo.san9@hotmail.com>
2023-11-01 17:48:32 -0300

    fix fee merchant details

--- src/services/types/register/merchants/merchantFeesConfig.interface.ts
+++ src/services/types/register/merchants/merchantFeesConfig.interface.ts
@@ -1,23 +1,30 @@
-
 export interface IMerchantFeesProps {
-  cashin_pix_fee_min?: number;
-  cashin_pix_fee_percent?: number;
-  cashin_pix_fee_plan_id?: number;
-  cashin_pix_fee_type?: string;
-  cashin_pix_fee_value?: number;
-  customer_withdraw_fee_min?: number;
-  customer_withdraw_fee_percent?: number;
-  customer_withdraw_fee_plan_id?: number;
-  customer_withdraw_fee_type?: string;
-  customer_withdraw_fee_value?: number;
-  merchant_withdraw_fee_min?: number;
-  merchant_withdraw_fee_percent?: number;
-  merchant_withdraw_fee_type?: string;
-  merchant_withdraw_fee_value?: number;
-  pix_refund_fee_min?: number;
-  pix_refund_fee_percent?: number;
-  pix_refund_fee_type?: string;
-  pix_refund_fee_value?: number;
+  merchant_withdraw_fee_type: string;
+  merchant_withdraw_fee_percent: number;
+  merchant_withdraw_fee_value: number;
+  merchant_withdraw_fee_min: number;
+  customer_withdraw_fee_type: string;
+  customer_withdraw_fee_percent: number;
+  customer_withdraw_fee_value: number;
+  customer_withdraw_fee_min: number;
+  cashin_pix_fee_type: string;
+  cashin_pix_fee_percent: number;
+  cashin_pix_fee_value: number;
+  cashin_pix_fee_min: number;
+  pix_refund_fee_type: string;
+  pix_refund_fee_percent: number;
+  pix_refund_fee_value: number;
+  pix_refund_fee_min: number;
+  fastpix_in_fee_type: string;
+  fastpix_in_fee_value: number;
+  fastpix_in_fee_percent: number;
+  fastpix_in_fee_min: number;
+  fastpix_refund_fee_type: string;
+  fastpix_refund_fee_value: number;
+  fastpix_refund_fee_percent: number;
+  fastpix_refund_fee_min: number;
+  customer_withdraw_fee_plan_id: null;
+  cashin_pix_fee_plan_id: null;
 }
 
 export interface MerchantFeesResponse {
