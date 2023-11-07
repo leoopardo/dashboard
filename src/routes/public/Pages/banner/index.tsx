d4c12d7de7d148a6bad167b2391e397ba6bc51c5
Leonardo Araujo <leo.san9@hotmail.com>
2023-11-07 09:59:18 -0300

    create gif page

--- src/routes/public/Pages/banner/index.tsx
+++ src/routes/public/Pages/banner/index.tsx
@@ -1,6 +1,55 @@
+import { Col, Row } from "antd";
+import { useMediaQuery } from "react-responsive";
+import Gif from "../../../../assets/gifPaybrokers.gif";
 
 export const Banner = () => {
+  const isEven200 = useMediaQuery({ maxHeight: "500px" });
   return (
-    <div>Banner</div>
-  )
-}
+    <Row
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#042645",
+        overflow: "hidden",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Col
+        span={24}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: isEven200 ? "-30vh" : undefined,
+        }}
+      >
+        <img
+          src={Gif}
+          style={{
+            maxHeight: "180vh",
+            maxWidth: "180vw",
+          }}
+        />
+      </Col>
+      {/* <motion.div
+        initial={{ width: "100%", opacity: 0.7 }}
+        animate={{
+          opacity: 1,
+          transition: { repeatType: "reverse", repeat: 10000, duration: 1 },
+        }}
+        style={{
+          position: "fixed",
+          top: "65vh",
+          left: "25vw",
+          overflow: "hidden",
+        }}
+      >
+        <Typography.Title style={{ wordBreak: "keep-all", width: "500px" }}>
+          Pagamento com segurança
+        </Typography.Title>
+      </motion.div> */}
+    </Row>
+  );
+};
