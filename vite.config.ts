8e232e6b144f44ceff9b6fe493e166cbc0f08202
Leonardo Araujo <leo.san9@hotmail.com>
2023-11-16 16:27:57 -0300

    add analytics test

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: { chunkSizeWarningLimit: 3000, },
-  
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: {
+        id: "G-8SMH6KZCSE",
+      },
+    }),
+  ],
+  build: { chunkSizeWarningLimit: 3000 },
+});
