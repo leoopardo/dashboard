c61f4978330663ecdaf9585b5cf2b57dd8b34bc2
Leonardo Araujo <leo.san9@hotmail.com>
2023-12-19 17:48:23 -0300

    fix moneyFormatter

--- src/utils/moneyFormatter.ts
+++ src/utils/moneyFormatter.ts
@@ -1,10 +1,10 @@
-function truncarNumero(num: number) {
-  const [inteiro, decimal] = num.toString().split(".");
-  return parseFloat(inteiro + "." + (decimal || "").slice(0, 2));
+function truncateNumber(num: number) {
+  const [int, dec] = num.toString().split(".");
+  return parseFloat(int + "." + (dec || "").slice(0, 2));
 }
 
 export function moneyFormatter(value: number) {
-  const number = truncarNumero(value);
+  const number = truncateNumber(value);
 
   return new Intl.NumberFormat("pt-BR", {
     style: "currency",
