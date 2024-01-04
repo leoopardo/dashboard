7d3b17ea60ddd80698f14272f75a2706b8d1092d
Leonardo Araujo <leo.san9@hotmail.com>
2024-01-04 11:54:33 -0300

    add - bank cards banners

---
+++ src/utils/BankBanners.ts
@@ -0,0 +1,19 @@
+import BitCapital from "@src/assets/banks/bit.png";
+import BS21 from "@src/assets/banks/bs2-1.png";
+import Bs2 from "@src/assets/banks/bs2.png";
+import BTG from "@src/assets/banks/btg.png";
+import Genial1 from "@src/assets/banks/genial-1.png";
+import Genial2 from "@src/assets/banks/genial-2.png";
+import Genial from "@src/assets/banks/genial.png";
+
+const BanksBanners = {
+  BTG,
+  Genial,
+  "Genial Filial 01": Genial1,
+  "Genial Filial 02": Genial2,
+  "Bs2 Filial 01": BS21,
+  teste: Bs2,
+  "Bit Capital": BitCapital,
+};
+
+export default BanksBanners;
