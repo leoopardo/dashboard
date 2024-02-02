a4dd42e112dce5915fa20f204180cec812c985eb
Leonardo Araujo <leo.san9@hotmail.com>
2024-02-02 10:10:31 -0300

    Fix: Add rule validation at edit self password

--- src/components/PageHeader/EditSelf/index.tsx
+++ src/components/PageHeader/EditSelf/index.tsx
@@ -86,7 +86,7 @@ export const EditSelfModal = ({
       }
     >
       {!body && SelfFetching ? (
-        <Spin size="large"/>
+        <Spin size="large" />
       ) : (
         <Form
           ref={formRef}
@@ -117,10 +117,14 @@ export const EditSelfModal = ({
             dependencies={["confirm"]}
             hasFeedback={body.password !== undefined}
             rules={[
-              { min: 8, message: t("input.min_of", { min: 8 }) || "" },
+              {
+                pattern:
+                  /^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[0-9a-zA-Z!@#$%^&*()_+]*$/,
+                message: `${t("input.password_type")}`,
+              },
               ({ getFieldValue }) => ({
                 validator(_, value) {
-                  if (!value || getFieldValue("confirm") === value) {
+                  if (!value || getFieldValue("confirmPasswprd") === value) {
                     setCantSubmit(false);
                     return Promise.resolve();
                   }
