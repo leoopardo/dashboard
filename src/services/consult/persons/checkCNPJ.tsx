8ba9ea3b2fc7b177c92f8f36c19641d27525811a
Leonardo Araujo <leo.san9@hotmail.com>
2024-03-18 16:30:52 -0300

    feature: legal persons page

---
+++ src/services/consult/persons/checkCNPJ.tsx
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { LegalPersonsItem } from "@src/services/types/register/legalPersons/persons.interface";
+import { useQuery } from "react-query";
+import { api } from "../../../config/api";
+
+export function useGetCheckCnpj(cnpj?: string) {
+  const { data, isFetching, error, refetch, isSuccess, remove } = useQuery<
+    LegalPersonsItem | null | undefined
+  >(
+    "CheckCnpj",
+    async () => {
+      const response = await api.get(`customer/check_cnpj/${cnpj}`, {});
+      return response.data;
+    },
+    {
+      refetchInterval: false,
+      refetchIntervalInBackground: false,
+      refetchOnMount: false,
+      keepPreviousData: false,
+    }
+  );
+
+  const CheckCnpjData = error ? undefined : data;
+  const isCheckCnpjDataFetching = isFetching;
+  const CheckCnpjDataError: any = error;
+  const CheckCnpjDataSuccess: any = isSuccess;
+  const refetchCheckCnpjData = refetch;
+  const removeCnpj = remove;
+  return {
+    CheckCnpjData,
+    CheckCnpjDataSuccess,
+    isCheckCnpjDataFetching,
+    CheckCnpjDataError,
+    refetchCheckCnpjData,
+    removeCnpj,
+  };
+}
