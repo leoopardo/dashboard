62421ecbf23785346ac967bdba245eaff95e689e
Leonardo Araujo <leo.san9@hotmail.com>
2024-03-19 16:37:38 -0300

    fix: add direct csv download

--- src/services/reports/register/organization/createUserReports.tsx
+++ src/services/reports/register/organization/createUserReports.tsx
@@ -2,15 +2,19 @@ import { queryClient } from "@src/services/queryClient";
 import { OrganizationUserQuery } from "@src/services/types/register/organization/organizationUsers.interface";
 import { useMutation } from "react-query";
 import { api } from "../../../../config/api";
+import { ReportsDataResponse } from "@src/services/types/reports/reports.interface";
 
 export function useCreateOrganizationReports(body: OrganizationUserQuery) {
-  const { isLoading, error, mutate, isSuccess } = useMutation<
-    OrganizationUserQuery | null | undefined
+  const { isLoading, error, mutate, isSuccess, data } = useMutation<
+  ReportsDataResponse | null | undefined
   >("CreateOrganizationReports", async () => {
-    const response = await api.post("report/csv/user/organization", {
-      ...body,
-    },
-    {});
+    const response = await api.post(
+      "report/csv/user/organization",
+      {
+        ...body,
+      },
+      {}
+    );
     await queryClient.refetchQueries({ queryKey: ["OrganizationUserReports"] });
     return response.data;
   });
@@ -19,11 +23,13 @@ export function useCreateOrganizationReports(body: OrganizationUserQuery) {
   const OrganizationReportsIsLoading = isLoading;
   const OrganizationReportsError = error;
   const OrganizationReportsIsSuccess = isSuccess;
+  const OrganizationReportsData = data;
 
   return {
     OrganizationReportsMutate,
     OrganizationReportsIsLoading,
     OrganizationReportsError,
     OrganizationReportsIsSuccess,
+    OrganizationReportsData,
   };
 }
